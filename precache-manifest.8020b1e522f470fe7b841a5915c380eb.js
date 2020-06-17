873d97aaecd1de1e534e7cb91320c819990ffdea
jonny <jturne202@caledonian.ac.uk>
2020-06-17 14:49:06 +0100

    Updates

---
+++ precache-manifest.8020b1e522f470fe7b841a5915c380eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "23eaccf500ca75c214ae29b1a23e2ea1",
+    "url": "/static/media/SearchForSelection.23eaccf5.PNG"
+  },
+  {
+    "revision": "f8518c8833e864c79aa0893d068c281b",
+    "url": "/static/media/Jonathan Turner CV 3.0.0.f8518c88.pdf"
+  },
+  {
+    "revision": "019921730be188987163c638af16b05b",
+    "url": "/static/media/GlasgowWeatherBot.01992173.png"
+  },
+  {
+    "revision": "5f3a6fd835c68fd657c502d0bbf9c4cf",
+    "url": "/static/media/ElectronScreenRecorder.5f3a6fd8.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6b6c05d6a593db96237e",
+    "url": "/static/js/main.f0a46be4.chunk.js"
+  },
+  {
+    "revision": "05d9fa857e8d045cef9c",
+    "url": "/static/js/2.6b9c2c12.chunk.js"
+  },
+  {
+    "revision": "6b6c05d6a593db96237e",
+    "url": "/static/css/main.260f9e3e.chunk.css"
+  },
+  {
+    "revision": "85cdb9d59595e75a03126b2e3ae34315",
+    "url": "/index.html"
+  }
+];
